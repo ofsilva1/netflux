b0ced52f00e0a0c339c245ed44f970cfb6f3a484
Omar Silva <omarsilva@Omars-MacBook-Air.local>
2023-06-19 11:25:40 -0600

    Initial commit

---
+++ src/main/java/guru/springframework/netflux/domain/Movie.java
@@ -0,0 +1,17 @@
+package guru.springframework.netflux.domain;
+
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+//@Builder
+public class Movie {
+    private String id;
+
+    @NonNull
+    private String title;
+}
